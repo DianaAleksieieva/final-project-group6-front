9a781223df91f875fb6b2228f7f317957c20a0e3
Denis-Ozolin <ozolindenx@gmail.com>
2022-01-15 01:35:39 +0200

    added header navigation & two svg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import css from './Header.module.css';
 import logo from '../../images/svg/logo.svg';
-import { options } from '../../db';
+// import { options } from '../../db';
 import { Nav } from '..';
 
 function Header() {
-  const { isLoggedIn } = options;
+  // const { isLoggedIn } = options;
   return (
     <section className={css.header}>
       <img src={logo} alt="Kapusta" />
-      {isLoggedIn && <Nav />}
+      <Nav />
+      {/* {isLoggedIn && <Nav />} */}
     </section>
   );
 }
