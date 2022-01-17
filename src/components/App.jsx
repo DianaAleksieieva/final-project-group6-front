cac41005ef24fc7f8ea056dabb07100ceb0cc595
Denis-Ozolin <ozolindenx@gmail.com>
2022-01-17 22:29:25 +0200

    added TransactionInput component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import css from './App.module.css';
-import { Header, Body, Footer, DayPicker, Droplist } from '.';
+import { Header, Body, Footer, DayPicker, TransactionInput } from '.';
 
 function App() {
   return (
     <div className={css.app}>
       <div className={css.container}>
         <Header />
+        <TransactionInput />
         <Body />
         {/* <DayPicker /> */}
-        {/* <Droplist /> */}
         <Footer />
       </div>
     </div>
