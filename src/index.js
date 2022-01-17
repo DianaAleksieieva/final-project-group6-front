c9afb0a4daae1a08e76eb290f5646f5d12bb9eb8
Oleksandr Yepishev <Alexandrdymka@gmail.com>
2022-01-17 17:43:40 +0200

    HomeView and LoginViev

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
-import './index.css';
 import { App } from './components';
 import { PersistGate } from 'redux-persist/integration/react';
+import './index.css';
+import './styles/base.css';
 
 ReactDOM.render(
   <React.StrictMode>
