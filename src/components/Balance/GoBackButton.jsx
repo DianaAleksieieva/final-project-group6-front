6c48737b3252d9135164596eb53a9355eb8187c9
Diana Aleksieieva <aleksodiana@gmail.com>
2022-01-19 01:26:10 +0200

    add navigation button

---
+++ src/components/Balance/GoBackButton.jsx
@@ -0,0 +1,13 @@
+import css from './Balance.module.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function GoBackButton() {
+  return (
+    <Link className={css.GoBackButton} to="/statistics">
+      Вернуться на главную
+    </Link>
+  );
+}
+
+export default GoBackButton;
--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -4,7 +4,7 @@ import authOperations from './auth-operations';
 const initialState = {
   user: { name: null, email: null },
   token: 'null',
-  isLoggedIn: true,
+  isLoggedIn: false,
   isRegistered: false,
 };
 
