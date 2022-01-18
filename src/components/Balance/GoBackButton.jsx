6799a17986aab2afe9eaf88e9f021eb265487ae2
Diana Aleksieieva <aleksodiana@gmail.com>
2022-01-19 01:32:47 +0200

    fix

--- src/components/Balance/GoBackButton.jsx
+++ src/components/Balance/GoBackButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function GoBackButton() {
   return (
-    <Link className={css.GoBackButton} to="/statistics">
+    <Link className={css.GoBackButton} to="/">
       Вернуться на главную
     </Link>
   );
