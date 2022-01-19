e0e17b12a3f724512e0ba18835c053d56fe3438f
Oleksandr Yepishev <Alexandrdymka@gmail.com>
2022-01-19 17:32:38 +0200

    перекомантил base url

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://final-project-group6-back.herokuapp.com/';
-axios.defaults.baseURL = 'http://localhost:4321/';
+axios.defaults.baseURL = 'https://final-project-group6-back.herokuapp.com/';
+// axios.defaults.baseURL = 'http://localhost:4321/';
 
 const token = {
   set(token) {
