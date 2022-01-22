933343505570943fc184714aa6bf5b33466dc893
Denis-Ozolin <ozolindenx@gmail.com>
2022-01-22 04:48:37 +0200

    change droplist

--- src/components/Droplist/Droplist.jsx
+++ src/components/Droplist/Droplist.jsx
@@ -1,49 +1,43 @@
-// import Select from 'react-select';
+import { useState, useRef, useEffect } from 'react';
 import css from './Droplist.module.css';
 
-// const customStyles = {
-//   option: (provided, state) => ({
-//     ...provided,
-//     margin: '0',
-//     padding: '10px 20px',
-//     border: '2px solid #F5F6FB',
-//     color: state.isSelected ? '#52555F' : '#C7CCDC',
-//     backgroundColor: state.isSelected ? '#F5F6FB' : '#FFFFFF',
-//   }),
-//   control: () => ({
-//     // none of react-select's styles are passed to <Control />
-//     margin: '0',
-//     padding: '8px 10px',
-//     height: 42,
-//   }),
-//   singleValue: (provided, state) => {
-//     const opacity = state.isDisabled ? 0.5 : 1;
-//     const transition = 'opacity 300ms';
+function Droplist({categories, categoryTitle, data, onChange}) {
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+  
+  useEffect(() => {
+    document.addEventListener("click", close);
+    return () => document.removeEventListener("click", close)
+  }, []);
 
-//     return { ...provided, opacity, transition };
-//   }
-// }
-
-// function Droplist({categories, categoryTitle}) {
-//   return (
-//       <Select 
-//         styles={customStyles}
-//         className={css.select}
-//         placeholder={categoryTitle}
-//         options={categories}
-//       />
-//   );
-// }
-
-function Droplist({ categories, categoryTitle }) {
-  return (
-    <select className={css.select} >
-      <option>{categoryTitle}</option>
-        { categories.map(({value, label}) => (
-          <option key={value}>{label}</option>
-        ))}
-    </select>
-  );
+  function close(e) {
+    setOpen(e && e.target === ref.current)
+  }
+  
+  return(
+    <>
+    <input id='category' type="hidden" value={data? data.value: ''} onChange={(e) => e.target.value = data}/>
+    <div className={css.dropdown}>
+      <div className={css.control} onClick={() => setOpen((prev) => !prev)}>
+        <div className={css.selectedValue} ref={ref} style={data && {color: '#52555F'}}>{data? data.label: categoryTitle}</div>
+        <div className={`${css.arrow} ${open ? css.open: null}`}></div>
+      </div>
+      <div className={`${css.options} ${open ? css.open: null}`}>
+        {categories.map((category) => (
+        <div   
+          key={category.value} 
+          className={`${css.option} ${data === category ? css.selected: null}`} 
+          onClick={() => {
+          onChange(category);
+          setOpen(false);
+        }}>
+          {category.label}
+        </div>
+        ))}    
+      </div>
+    </div>
+    </>
+  )
 }
-
+  
 export default Droplist;
