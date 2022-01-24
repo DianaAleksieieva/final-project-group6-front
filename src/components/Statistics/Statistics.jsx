ac31324c088db5c27e6adc7316930a66f63c223d
AlexTvardGit <alex.tvard.git@gmail.com>
2022-01-24 21:04:16 +0200

    upd
    -update category variable
    -add text when category empty

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,12 +4,12 @@ import ButtonChangeCategories from './ButtonChangeCategories';
 import StatisticsCategoies from './StatisticsCategories/StatisticCategories';
 import { EXPENCES, INCOMES } from '../../constans';
 import Charts from '../Charts/Charts';
+import { ReactComponent as Img } from '../../images/svg/dollar.svg';
 
 export default function Statistics({ month, year }) {
   const [active, setActive] = useState('Расход');
   const [transactionType, setTransactionType] = useState(EXPENCES);
-  const [category, setCategory] = useState(''); // ЭТА ПЕРЕМЕННАЯ ДЛЯ ГРАФИКОВ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  console.log("category: ", category);
+  const [category, setCategory] = useState('');
 
   useEffect(() => {
     if (active === 'Доход') {
@@ -22,8 +22,10 @@ export default function Statistics({ month, year }) {
   const changeStatus = () => {
     if (active === 'Доход') {
       setActive('Расход');
+      setCategory('');
     } else if (active === 'Расход') {
       setActive('Доход');
+      setCategory('');
     }
   };
 
@@ -31,10 +33,17 @@ export default function Statistics({ month, year }) {
     setCategory(categoryToChange);
   };
 
+  function categoryIsEmpty() {
+    return category === '';
+  }
+
   return (
     <>
       <div className={css.containerButton}>
-        <ButtonChangeCategories active={active} changeStatus={changeStatus} />
+        <ButtonChangeCategories
+          active={active}
+          changeStatus={changeStatus}
+        />
         <StatisticsCategoies
           month={month}
           year={year}
@@ -43,7 +52,20 @@ export default function Statistics({ month, year }) {
         />
       </div>
       <div className={css.containerGraph}>
-        <Charts category={category} month={month} year={year} />
+        {
+          categoryIsEmpty() && (
+            <div>
+              <Img height={100} width={100} />
+              <h2>Для отображения графиков - пополните баланс на 100$</h2>
+              <h3>Шутка!Просто выберите категорию выше</h3>
+            </div>
+          )
+
+        }
+        {
+          !categoryIsEmpty() &&
+          <Charts category={category} month={month} year={year} />
+        }
       </div>
     </>
   );
