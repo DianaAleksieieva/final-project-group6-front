f145af4144a9eedaa10a2cf5eb680307f33a3b75
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2022-01-25 13:38:27 +0200

    fix background satistic icon

--- src/components/Statistics/StatisticsCategories/StatisticCategories.jsx
+++ src/components/Statistics/StatisticsCategories/StatisticCategories.jsx
@@ -2,78 +2,101 @@ import css from './StatisticCategories.module.css';
 import iconsContainer from '../../../utils/statisticComponents';
 import { useEffect, useState } from 'react';
 import { getByTypeMonthly } from '../../../api/transactionsAPI';
+import { ReactComponent as BgStatisric } from '../../../images/svg/statistics/bg-statisric-ico.svg';
 
-export default function StatisticCategories({ transactionType, month, year, changeCategory }) {
-    const { type } = transactionType;
-    const [monthTransactionsByType, setMonthTransactionsByType] = useState([]);
+export default function StatisticCategories({
+  transactionType,
+  month,
+  year,
+  changeCategory,
+}) {
+  const { type } = transactionType;
+  const [monthTransactionsByType, setMonthTransactionsByType] = useState([]);
+  const [activeBackground, setActiveBackground] = useState(null);
 
-    async function fetchMonthlyDataLoc(type, year, month) {
-        const data = await getByTypeMonthly({ type, year, month });
-        return data.categories;
+  async function fetchMonthlyDataLoc(type, year, month) {
+    const data = await getByTypeMonthly({ type, year, month });
+    return data.categories;
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchMonthlyDataLoc(type, year, month);
+      setMonthTransactionsByType(data);
     }
+    fetchData();
+  }, [month, type, year]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const data = await fetchMonthlyDataLoc(type, year, month);
-            setMonthTransactionsByType(data);
-        }
-        fetchData();
-    }, [month, type, year]);
+  function getResultArray() {
+    let newArr = [];
+    const keys = Object.keys(monthTransactionsByType);
+    const values = Object.values(monthTransactionsByType);
 
-    function getResultArray() {
-        let newArr = [];
-        const keys = Object.keys(monthTransactionsByType);
-        const values = Object.values(monthTransactionsByType);
+    keys.forEach(function (item, i) {
+      newArr.push({ CatName: item, Amount: values[i].sum });
+    });
 
-        keys.forEach(function (item, i) {
-            newArr.push({ CatName: item, Amount: values[i].sum });
+    return newArr;
+  }
+
+  // console.log('result', getResultArray());
+
+  function res(iconsContainer) {
+    let arr = [];
+
+    if (monthTransactionsByType) {
+      const result = getResultArray();
+
+      if (result && result !== []) {
+        result.map(item =>
+          arr.push(
+            ...iconsContainer.filter(function (el) {
+              return el.CatName === item.CatName;
+            }),
+          ),
+        );
+
+        arr = arr.map(function (item, i) {
+          return (item = { ...item, ...result[i] });
         });
-
-        return newArr;
+      }
     }
 
-    // console.log('result', getResultArray());
+    return arr;
+  }
 
-    function res(iconsContainer) {
-        let arr = [];
-
-        if (monthTransactionsByType) {
-            const result = getResultArray();
-
-            if (result && result !== []) {
-                result.map((item) => (
-                    arr.push(...iconsContainer.filter(function (el) {
-                        return el.CatName === item.CatName
-                    }))
-
-                ))
-
-                arr = arr.map(function (item, i) {
-                    return item = { ...item, ...result[i] };
-                })
-            }
-        }
-
-        return arr;
-    }
-
-    return (
-        <div>
-            <ul className={css.statistics_list}>
-                {
-                    res(iconsContainer).map((item, index) => (
-                        <li className={css.statistics_item} key={index}>
-                            <span className={css.labels_style}>{item.Amount}</span>
-                            <button className={css.statistics_icon} onClick={() => changeCategory(item.CatName)}>
-                                {item.Component}
-                            </button>
-                            <span className={css.labels_style}>{item.CatLabel}</span>
-                        </li>
-
-                    ))
+  return (
+    <div>
+      <ul className={css.statistics_list}>
+        {res(iconsContainer).map((item, index) => (
+          <li className={css.statistics_item} key={index}>
+            <span className={css.labels_style}>{item.Amount}</span>
+            <div className={css.wrapper_icon}>
+              <BgStatisric
+                className={
+                  activeBackground === index
+                    ? css.wrapper_background
+                    : css.background_hide
                 }
-            </ul >
-        </div>
-    )
+              />
+              <button
+                className={
+                  activeBackground !== index
+                    ? css.statistics_icon
+                    : css.statistics_icon_active
+                }
+                onClick={() => {
+                  changeCategory(item.CatName);
+                  setActiveBackground(index);
+                }}
+              >
+                {item.Component}
+              </button>
+            </div>
+            <span className={css.labels_style}>{item.CatLabel}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
